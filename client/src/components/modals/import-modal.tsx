627b40ca757178e599a0367bdb73197f30ace960
Alice <63879236+Benji42@users.noreply.github.com>
2024-02-08 14:44:29 -0500

    Fixes to import modal

--- client/src/components/modals/import-modal.tsx
+++ client/src/components/modals/import-modal.tsx
@@ -29,8 +29,20 @@ export const ImportModal = ({setOpen, onClose}: Props) => {
 				<AlertDialog.Content className={css.content}>
 					<AlertDialog.Title className={css.title}>Deck Importing</AlertDialog.Title>
 					<AlertDialog.Description asChild className={css.description}>
-						<div className={css.cardListBox}>
-							<input value={code} onChange={(e) => setCode(e.target.value)}></input>
+						<div>
+							<div>
+								To import a deck, all you need to do is supply the deck code of another scuffed
+								deck. You cannot import decks from the HC TCG website. This will not override decks
+								if you import a deck with the same name of one you already have.
+							</div>
+							<div>
+								<input
+									placeholder={'Deck code...'}
+									value={code}
+									onChange={(e) => setCode(e.target.value)}
+									className={css.importBar}
+								></input>
+							</div>
 						</div>
 					</AlertDialog.Description>
 					<div className={css.buttonContainer}>
